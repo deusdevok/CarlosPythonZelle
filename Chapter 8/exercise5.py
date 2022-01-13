64cd867b59036aeeddc396c5354e2e4efd7d577d
bionic <carlosaprado@gmail.com>
2022-01-13 17:49:28 -0300

    chapter 8 exs 3-7

--- Chapter 8/exercise5.py
+++ Chapter 8/exercise5.py
@@ -1,5 +1,26 @@
+# Determines if n is prime.
+
+import math
+
 def main():
-	pass
+	try:
+		n = int(input('Enter a positive whole number greater than 2: '))
+	except ValueError:
+		print('n must be positive integer...')
+		return
+
+	if n <= 2:
+		print('The number must be greater than 2')
+		return
+
+	d = 2
+	while d <= math.sqrt(n):
+		if n%d == 0:
+			print('{} is not prime'.format(n))
+			return
+		d += 1
+
+	print('{} is prime!'.format(n))
 
 if __name__ == '__main__':
 	main()
