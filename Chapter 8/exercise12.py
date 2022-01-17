d96ef42bc1eca27134ac586a49ded188597554a6
bionic <carlosaprado@gmail.com>
2022-01-17 13:31:42 -0300

    chapter 8 more exs

--- Chapter 8/exercise12.py
+++ Chapter 8/exercise12.py
@@ -1,5 +1,26 @@
 def main():
-	pass
+
+	HDD = 0 # Heating Degree Days
+	CDD = 0 # Cooling Degree Days
+
+	fileName = input('Enter the file name: ')
+	infile = open(fileName, 'r')
+	line = infile.readline() # Read first line
+
+
+	while line != '':
+
+		d = float(line)
+
+		if d < 60:
+			HDD += 60-d
+		elif d > 80:
+			CDD += d-80
+
+		line = infile.readline()
+
+	print('HDD: {:.2f}'.format(HDD))
+	print('CDD: {:.2f}'.format(CDD))
 
 if __name__ == '__main__':
 	main()
