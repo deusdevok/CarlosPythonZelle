e152388f32f1f30d4f22eb97ba001637ad006c51
bionic <carlosaprado@gmail.com>
2022-01-21 11:56:12 -0300

    finish chapter 8

--- Chapter 8/exercise14.py
+++ Chapter 8/exercise14.py
@@ -1,5 +1,35 @@
+# Converts a color image to grayscale
+
+from graphics import *
+
 def main():
-	pass
+	nameFile = input('Enter the name of the image: ')
+
+	win = GraphWin('Image To Grayscale', 800, 800)
+
+	myImage = Image(Point(400, 400), nameFile)
+
+	nCols = myImage.getWidth()
+	nRows = myImage.getHeight()
+
+	myImage.draw(win)
+
+	for row in range(nRows):
+		for col in range(nCols):
+			r, g, b = myImage.getPixel(col, row)
+			brightness = int(round(0.299*r + 0.587*g + 0.114*b))
+			myImage.setPixel(col, row, color_rgb(brightness, brightness, brightness))
+		myImage.undraw()
+		myImage.draw(win)
+
+	myImage.undraw()
+	myImage.draw(win)
+
+	nameSaveFile = input('Enter the name of the saved grayscale image: ')
+	myImage.save(nameSaveFile)
+
+	win.getMouse()
+	win.close()
 
 if __name__ == '__main__':
 	main()
--- Chapter 8/exercise15.py
+++ Chapter 8/exercise15.py
@@ -1,5 +1,30 @@
+# Converts a color image to its color negative
+from graphics import *
+
 def main():
-	pass
+	nameFile = input('Enter the name of the image: ')
+
+	win = GraphWin('Image To color negative', 800, 800)
+
+	myImage = Image(Point(400, 400), nameFile)
+
+	nCols = myImage.getWidth()
+	nRows = myImage.getHeight()
+
+	myImage.draw(win)
+
+	for row in range(nRows):
+		for col in range(nCols):
+			r, g, b = myImage.getPixel(col, row)
+			myImage.setPixel(col, row, color_rgb(255-r, 255-g, 255-b))
+		update()
+
+
+	nameSaveFile = input('Enter the name of the saved color negative image: ')
+	myImage.save(nameSaveFile)
+
+	win.getMouse()
+	win.close()
 
 if __name__ == '__main__':
 	main()
